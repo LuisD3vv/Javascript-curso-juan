b62b8922d59dc6c8492af7f39251fa8b55ed895b
Lissandrodev <Luismexico2006@gmail.com>
2025-09-21 16:40:42 -0700

    mitad proyecto festival

--- Festival Musical/gulpfile.js
+++ Festival Musical/gulpfile.js
@@ -1,20 +1,21 @@
-import { src, dest } from 'gulp'
+import { src, dest, watch } from 'gulp'
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
 
 
 const sass = gulpSass(dartSass);
 
+// funcion que al ejecutarse, crea un documento y compila el css
+//despues ejecuta la funcion saas
 export function css (done) {
-    src('src/scss/app.scss')
-        .pipe( sass() )
+    src ('src/scss/app.scss')
+        .pipe( sass().on('error', sass.logError))
         .pipe(dest('dist/css'));
-
     done();
 }
-// export function hola (done) {
-//     console.log("Hola desde gulp perro programador");
-//
-//     done(); // ya finalizo la ejecucion del codigo
-// }
+// funcion para observar los cambios de los archivos en tiempo real
+// con el parametro de gulp --watch
+export function dev(){
+    watch('src/scss/**/*.scss', css);
 
+}
